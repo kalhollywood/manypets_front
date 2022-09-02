0dede60906df94cd8140f646d97d675003408267
kalhollywood <kal.hollywood@gmail.com>
2022-09-02 11:39:59 +0100

    basic front end form set up

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { FormControl, TextField, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Get your quote!</h1>
+      <p>Please enter your pet's details below:</p>
+      <FormControl fullWidth>
+        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="dog"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value="Dog" control={<Radio />} label="Dog" />
+          <FormControlLabel value="Cat" control={<Radio />} label="Cat" />
+
+        </RadioGroup>
+        <p>Your pet's age</p>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" required />
+        <p>Breed</p>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" required />
+        <p>Address</p>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" required />
+      </FormControl>
+    </div>
+  );
+}
+
+export default App;
